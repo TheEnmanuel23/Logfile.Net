938eec683e9c6cddcc2635b5fbabbe682323e52b
Enmanuel Isaac Jarquin Mercado <ejarquin@tda.thedataage.com>
2017-06-28 12:10:04 -0600

    realize prueba del logfile avanzado

--- Logfile/LogfileTest/AdvancedWritingLogTest.cs
+++ Logfile/LogfileTest/AdvancedWritingLogTest.cs
@@ -14,6 +14,20 @@ namespace LogfileTest
             Loginfo info = null;
             writeLog.Log(info);
             Assert.AreNotEqual(StreamWriter.Null, writeLog.Logfile);
+        } 
+
+        [TestMethod]
+        public void TestSuccess()
+        {
+            var writeLog = new WriteInDiskLogfile();
+            Loginfo info = new Loginfo();
+            info.ControlName = "Mi control";
+            info.ErrorLineNum = 12;
+            info.ExceptionName = "Error de division entre cero";
+            info.MainName = "Oficina virtual";
+            info.EventName = "Main oficina virtual";
+            writeLog.Log(info);
+            Assert.AreNotEqual(StreamWriter.Null, writeLog.Logfile);
         }
     }
 }
